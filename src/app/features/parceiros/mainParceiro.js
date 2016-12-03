372eb1a147dc336b07cf492373505138f1f3b320
LacosEFlores <robertasoares1997@gmail.com>
2016-12-03 17:48:05 -0200

    opa foi

--- src/app/features/parceiros/mainParceiro.js
+++ src/app/features/parceiros/mainParceiro.js
@@ -1,12 +1,22 @@
 angular.module('grupoLacosFloresFront').controller('parceirosController', function($scope, parceirosService){
   $scope.testaMensagem = 'eita XD';
+  
   $scope.floricultura = {
-    nome: "",
-    email: "",
-    cnpj: "",
-    endereco: {}
+    id: '',
+    razaoSocial: '',
+    nomeFantasia: '',
+    cnpj: '',
+    cep: '',
+    logradouro: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    /* endereco: {} */
   };
-  $scope.floricultura.endereco.cep = "";
+
+  $scope.parceiro = [];
+
+  /* $scope.floricultura.endereco.cep = "";
 
   $scope.consultaCEP = function(cep){
     if( cep.length < 8 )
@@ -30,5 +40,49 @@ angular.module('grupoLacosFloresFront').controller('parceirosController', functi
         url: 'http://viacep.com.br/ws/'+cep+'/json/'
       });
     }
-  }
-});
+  } */
+
+
+    $scope.listarParceiro = function(){
+       parceirosService.listarParceiro().then(function(retorno){
+            $scope.parceiro = retorno.data
+        }).catch(function(){
+            console.log('deu ruim');
+        })
+    }
+
+    $scope.listarParceiro();
+
+    $scope.salvarParceiro = function(){
+        
+        var parceiross = JSON.stringify($scope.formParceiro);
+
+        parceirosService.cadastrarParceiro(parceiross).then(function(retorno){
+            console.log('DEU BOM')
+        }).catch({
+
+        });
+    }
+
+})
+.service('parceirosService', function($http){
+    return{
+        cadastrarParceiro: function(parceiross){
+
+
+            return $http({
+                method: 'POST',
+                url: '',
+                data: parceiross
+            });
+        },
+
+        listarParceiro: function(){
+            return $http({
+                method: 'GET',
+                url: ''
+            })
+        }
+    } 
+})
+;
